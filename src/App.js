cbf68d3e5e468f58dea62512f4c280ff46abf0ff
Tom Vincent <git@tlvince.com>
2017-07-13 14:02:43 +0100

    revert replaceReducer hack
    This reverts commit 94414d1374b05bcaf6cbd762b196554d6026f191.

--- src/App.js
+++ src/App.js
@@ -2,22 +2,12 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { incrementCount } from './app-reducer'
 
-import rootReducer from './root-reducer'
-
-const App = ({app, incrementCount}) => {
-  const handleClick = () => {
-    window.VAN.store.replaceReducer(rootReducer)
-    incrementCount()
-  }
-
-  return (
-    <div>
-      <h1>React</h1>
-      <pre>{JSON.stringify(app, null, 2)}</pre>
-      <button onClick={handleClick}>Increment</button>
-    </div>
-  )
-}
+const App = ({app, incrementCount}) =>
+  <div>
+    <h1>React</h1>
+    <pre>{JSON.stringify(app, null, 2)}</pre>
+    <button onClick={incrementCount}>Increment</button>
+  </div>
 
 const mapStateToProps = ({app}) => ({app})
 
--- src/Root.js
+++ src/Root.js
@@ -2,9 +2,10 @@ import React from 'react'
 import { Provider } from 'react-redux'
 
 import App from './App'
+import store from './store'
 
 const Root = () =>
-  <Provider store={window.VAN.store}>
+  <Provider store={store}>
     <App />
   </Provider>
 
